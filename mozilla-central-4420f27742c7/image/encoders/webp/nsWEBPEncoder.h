5bdc7195a33abfce6dc0c29fc62737086b1fc73a
Matheus Morais <matheus.morais@gmail.comm>
2013-04-20 17:06:15 -0300

    Inclusão do encoder para webp

---
+++ mozilla-central-4420f27742c7/image/encoders/webp/nsWEBPEncoder.h
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+   * License, v. 2.0. If a copy of the MPL was not distributed with this
+   * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+  
+#include "mozilla/Attributes.h"
+#include "mozilla/ReentrantMonitor.h"
+  
+#include "imgIEncoder.h"
+
+#define NS_WEBPENCODER_CID \
+{
+ /* 05848c32-1722-462d-bb49-688dd1a63ee5 */	   \
+ 0x05848c32,					   \
+ 0x1722,					   \
+ 0x462d,					   \
+ {0xbb, 0x49, 0x68, 0x8d, 0xd1, 0xa6, 0x3e, 0xe5 } \
+}
+
