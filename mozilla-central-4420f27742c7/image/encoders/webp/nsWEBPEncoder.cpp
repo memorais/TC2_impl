6538cc6bc3b35d856685a8a3ab9fe9088b8efd52
Matheus Morais <matheus.morais@gmail.comm>
2013-05-18 15:12:56 -0300

    Implementação inicial do Encoding

--- mozilla-central-4420f27742c7/image/encoders/webp/nsWEBPEncoder.cpp
+++ mozilla-central-4420f27742c7/image/encoders/webp/nsWEBPEncoder.cpp
@@ -9,3 +9,159 @@
 #include "nsStreamUtils.h"
 
 using namespace mozilla;
+
+NS_IMPL_THREADSAFE_ISUPPORTS3(nsWEBPEncoder, imgIEncoder, nsIInputStream, nsIAsyncInputStream)
+
+nsWEBPEncoder::nsWEBPEncoder() : mImageBuffer(nullptr), mImageBufferSize(0),
+                                 mImageBufferUsed(0), mImageBufferReadPoint(0),
+                                 mCallback(nullptr),
+                                 mCallbackTarget(nullptr), mNotifyThreshold(0),
+                                 mReentrantMonitor("nsWEBPEncoder.mReentrantMonitor")
+{
+}
+
+nsWEBPEncoder::~nsWEBPEncoder()
+{
+  if (mImageBuffer) {
+    moz_free(mImageBuffer);
+    mImageBuffer = nullptr;
+  }
+}
+
+// Returns the number of bytes in the image buffer used.
+NS_IMETHODIMP nsWEBPEncoder::GetImageBufferUsed(uint32_t *aOutputSize)
+{
+  NS_ENSURE_ARG_POINTER(aOutputSize);
+  *aOutputSize = mImageBufferUsed;
+  return NS_OK;
+}
+
+// Returns a pointer to the start of the image buffer
+NS_IMETHODIMP nsWEBPEncoder::GetImageBuffer(char **aOutputBuffer)
+{
+  NS_ENSURE_ARG_POINTER(aOutputBuffer);
+  *aOutputBuffer = reinterpret_cast<char*>(mImageBuffer);
+  return NS_OK;
+}
+
+NS_IMETHODIMP nsWEBPEncoder::Close()
+{
+  if (mImageBuffer != nullptr) {
+    moz_free(mImageBuffer);
+    mImageBuffer = nullptr;
+    mImageBufferSize = 0;
+    mImageBufferUsed = 0;
+    mImageBufferReadPoint = 0;
+  }
+  return NS_OK;
+}
+
+NS_IMETHODIMP nsWEBPEncoder::Available(uint64_t *_retval)
+{
+  if (!mImageBuffer)
+    return NS_BASE_STREAM_CLOSED;
+
+  *_retval = mImageBufferUsed - mImageBufferReadPoint;
+  return NS_OK;
+}
+
+NS_IMETHODIMP nsWEBPEncoder::Read(char * aBuf, uint32_t aCount,
+                                 uint32_t *_retval)
+{
+  return ReadSegments(NS_CopySegmentToBuffer, aBuf, aCount, _retval);
+}
+
+/* [noscript] unsigned long readSegments (in nsWriteSegmentFun aWriter, in voidPtr aClosure, in unsigned long aCount); */
+NS_IMETHODIMP nsWEBPEncoder::ReadSegments(nsWriteSegmentFun aWriter, void *aClosure, uint32_t aCount, uint32_t *_retval)
+{
+  // Avoid another thread reallocing the buffer underneath us
+  ReentrantMonitorAutoEnter autoEnter(mReentrantMonitor);
+
+  uint32_t maxCount = mImageBufferUsed - mImageBufferReadPoint;
+  if (maxCount == 0) {
+    *_retval = 0;
+    return mFinished ? NS_OK : NS_BASE_STREAM_WOULD_BLOCK;
+  }
+
+  if (aCount > maxCount)
+    aCount = maxCount;
+  nsresult rv = aWriter(this, aClosure,
+                        reinterpret_cast<const char*>(mImageBuffer+mImageBufferReadPoint),
+                        0, aCount, _retval);
+  if (NS_SUCCEEDED(rv)) {
+    NS_ASSERTION(*_retval <= aCount, "bad write count");
+    mImageBufferReadPoint += *_retval;
+  }
+
+  // errors returned from the writer end here!
+  return NS_OK;
+}
+
+NS_IMETHODIMP nsWEBPEncoder::IsNonBlocking(bool *_retval)
+{
+  *_retval = true;
+  return NS_OK;
+}
+
+NS_IMETHODIMP nsWEBPEncoder::AsyncWait(nsIInputStreamCallback *aCallback,
+                                      uint32_t aFlags,
+                                      uint32_t aRequestedCount,
+                                      nsIEventTarget *aTarget)
+{
+  if (aFlags != 0)
+    return NS_ERROR_NOT_IMPLEMENTED;
+
+  if (mCallback || mCallbackTarget)
+    return NS_ERROR_UNEXPECTED;
+
+  mCallbackTarget = aTarget;
+  // 0 means "any number of bytes except 0"
+  mNotifyThreshold = aRequestedCount;
+  if (!aRequestedCount)
+    mNotifyThreshold = 1024; // We don't want to notify incessantly
+
+  // We set the callback absolutely last, because NotifyListener uses it to
+  // determine if someone needs to be notified.  If we don't set it last,
+  // NotifyListener might try to fire off a notification to a null target
+  // which will generally cause non-threadsafe objects to be used off the main thread
+  mCallback = aCallback;
+
+  // What we are being asked for may be present already
+  NotifyListener();
+  return NS_OK;
+}
+
+NS_IMETHODIMP nsWEBPEncoder::CloseWithStatus(nsresult aStatus)
+{
+  return Close();
+}
+
+void
+nsWEBPEncoder::NotifyListener()
+{
+  // We might call this function on multiple threads (any threads that call
+  // AsyncWait and any that do encoding) so we lock to avoid notifying the
+  // listener twice about the same data (which generally leads to a truncated
+  // image).
+  ReentrantMonitorAutoEnter autoEnter(mReentrantMonitor);
+
+  if (mCallback &&
+      (mImageBufferUsed - mImageBufferReadPoint >= mNotifyThreshold ||
+       mFinished)) {
+    nsCOMPtr<nsIInputStreamCallback> callback;
+    if (mCallbackTarget) {
+      callback = NS_NewInputStreamReadyEvent(mCallback, mCallbackTarget);
+    } else {
+      callback = mCallback;
+    }
+
+    NS_ASSERTION(callback, "Shouldn't fail to make the callback");
+    // Null the callback first because OnInputStreamReady could reenter
+    // AsyncWait
+    mCallback = nullptr;
+    mCallbackTarget = nullptr;
+    mNotifyThreshold = 0;
+
+    callback->OnInputStreamReady(this);
+  }
+}
--- mozilla-central-4420f27742c7/image/encoders/webp/nsWEBPEncoder.h
+++ mozilla-central-4420f27742c7/image/encoders/webp/nsWEBPEncoder.h
@@ -16,12 +16,43 @@
 	0x462d,							\
        {0xbb, 0x49, 0x68, 0x8d, 0xd1, 0xa6, 0x3e, 0xe5} 	\
 }
-/*
+
 extern "C" {
 #include "webp/encode.h"
 }
 
 class nsWEBPEncoder MOZ_FINAL : public imgIEncoder
 {
+  typedef mozilla::ReentrantMonitor ReentrantMonitor;
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_IMGIENCODER
+  NS_DECL_NSIINPUTSTREAM
+  NS_DECL_NSIASYNCINPUTSTREAM
 
-}*/
+  nsWEBPEncoder();
+
+private:
+  ~nsWEBPEncoder();
+
+protected:
+
+  // image buffer
+  uint8_t* mImageBuffer;
+  uint32_t mImageBufferSize;
+  uint32_t mImageBufferUsed;
+
+  uint32_t mImageBufferReadPoint;
+
+  nsCOMPtr<nsIInputStreamCallback> mCallback;
+  nsCOMPtr<nsIEventTarget> mCallbackTarget;
+  uint32_t mNotifyThreshold;
+
+  /*
+    nsWEBPEncoder is designed to allow one thread to pump data into it while another
+    reads from it.  We lock to ensure that the buffer remains append-only while
+    we read from it (that it is not realloced) and to ensure that only one thread
+    dispatches a callback for each call to AsyncWait.
+   */
+  ReentrantMonitor mReentrantMonitor;
+};
